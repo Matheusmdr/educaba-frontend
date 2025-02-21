bccc0459bc101bd90fa3182d42eb8831acd0a5a4
Matheusmdr <matheus.m.rocha@unesp.br>
2025-02-21 00:46:44 -0300

    fix: resolve error in env url validation

--- src/env.js
+++ src/env.js
@@ -20,7 +20,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_API_HOST: z.string().url(),
+    NEXT_PUBLIC_API_HOST: z.string(),
   },
 
   /**
